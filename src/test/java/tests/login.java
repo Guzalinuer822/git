71dcf60585a74a883ea2538c3854dcf032e1d945
Guzalinuer822 <57195528+Guzalinuer822@users.noreply.github.com>
2019-12-16 21:21:30 +0100

    from github sameline

--- src/test/java/tests/login.java
+++ src/test/java/tests/login.java
@@ -5,7 +5,7 @@ public class login {
     public static void main(String[] args) {
         System.out.println("I am running first time");
 
-
+        System.out.println("******* line 8 at Github *********");
 
         System.out.println("Now I am writing second time in IntelliJ");
 
