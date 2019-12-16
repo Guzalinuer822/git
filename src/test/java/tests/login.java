16bcdbc8e16a61df65972cb1146b98f0da5a0102
guzal <myEmail@Gmail.com>
2019-12-16 19:49:51 +0100

    second time

--- src/test/java/tests/login.java
+++ src/test/java/tests/login.java
@@ -4,5 +4,8 @@ public class login {
 
     public static void main(String[] args) {
         System.out.println("I am running first time");
+
+
+        System.out.println("Now I am writing second time in IntelliJ");
     }
 }
